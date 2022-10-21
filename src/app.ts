5e404530412efb34422ebebdbf47c03bf65bc9c2
raverena <raverena96@gmail.com>
2022-10-21 14:08:37 +0900

    [Add] mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,22 @@
-import express, { type Request, Response, NextFunction } from "express";
-import path from "path";
 import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.join(__dirname, "./config.env") });
+
+import express, { type Request, Response, NextFunction } from "express";
+
 import morgan from "morgan";
 import logger, { stream } from "./utils/winston";
-
-dotenv.config(); // ({ path: path.join(__dirname, './env/server.env') });//dotenv : env 로드 모듈 server.env 환경변수 가져오기
+import connectDB from "./config/db";
 
 const app = express();
-const port = 5000; // process.env.port
+const port = process.env.PORT || 5000; // process.env.port
 const combined =
   ':remote-addr - :remote-user ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"';
 // 기존 combined 포멧에서 timestamp만 제거
 const morganFormat = process.env.NODE_ENV !== "production" ? "dev" : combined; // NOTE: morgan 출력 형태 server.env에서 NODE_ENV 설정 production : 배포 dev : 개발
+
+app.use(express.json());
+connectDB();
 
 app.use(morgan(morganFormat, { stream: stream })); // morgan 로그 설정
 
